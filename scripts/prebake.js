9f9ad8083e8cc03270c8118aa1fec72d08339138
Ying Chyi Gooi <25574844+YingCGooi@users.noreply.github.com>
2026-02-02 16:32:36 -0600

    add documentation for useSpectrum

--- scripts/prebake.js
+++ scripts/prebake.js
@@ -1,82 +1,108 @@
 // CSS overrides
-// @name: useHue
+// @name: wetEditor
+// @param: <Number> saturation amount (default 3)
+// @param: <Number> hue shift (default -15)
 // @example: useHue(3, -15)
-window.useHue = function (amt=3,hue=-15) {
+window.wetEditor = function (amount=3,hueShift=-15) {
   document.querySelectorAll('style').forEach(n => {
     n.append('[type=range]{width:400px !important;accent-color:oklch(.7 .24 240);}')
     n.append(':root { --background: #001 !important} ')
-    n.append('canvas {filter:saturate(' +amt+ ')}')  
+    n.append('canvas {filter:saturate(' +amount+ ')}')  
     n.append('#code .cm-line>*{background: #0000;}')
-    n.append('.cm-line{filter:hue-rotate(' +hue+ 'deg) saturate(' +amt+ ')}')
+    n.append('.cm-line{filter:hue-rotate(' +hueShift+ 'deg) saturate(' +amount+ ')}')
   })
 }
 // CSS helper function
-function lineargradient(
-  stops = [],  
-  steps = 7,
-  freq=8000, next=1/2, alpha=1,
-  hue=40, hueStep=40, 
-  w=.1, wmul=.1,
-  mode='to bottom',
-) {
+function lineargradient(stops = [], steps, freq, next, alpha, hue, hueStep, w, wmul = 0.1, mode = 'to bottom') {
   const freqStops = {
     [16000]: 4.4,
-    [8000] : 12.3,
-    [4000] : 20.6, // 5 *32 2^5
-    [2000] : 28.9, // 4 *16 2^4
-    [1000] : 37.2, // 3 *8 2^3 => 3 * -8.29
-    [500]  : 45.4, // 2 *4 2^2 => 2 * -8.29
-    [250]  : 53.7, // 1 *2 2^1 => -8.29
-    [125]  : 62,   // use as base
-    [62.5] : 70.3,
-  }
+    [8000]: 12.3,
+    [4000]: 20.6, // 5 *32 2^5
+    [2000]: 28.9, // 4 *16 2^4
+    [1000]: 37.2, // 3 *8 2^3 => 3 * -8.29
+    [500]: 45.4, // 2 *4 2^2 => 2 * -8.29
+    [250]: 53.7, // 1 *2 2^1 => -8.29
+    [125]: 62, // use as base
+    [62.5]: 70.3,
+  };
+  // only construct a stops array if not provided
   if (stops.length === 0) {
-    for (let i=0; i<steps; i++) {
-      let width = next < 1 ? w+(wmul*i) : w*steps-(wmul*i)
-      stops.push([
-        freq, 'oklch(.7 .2 '+hue+'/'+alpha+')', width
-      ])
-      hue += hueStep
-      freq *= next
+    for (let i = 0; i < steps; i++) {
+      let width = next < 1 ? w + wmul * i : w * steps - wmul * i;
+      stops.push([freq, 'oklch(.7 .2 ' + hue + '/' + alpha + ')', width]);
+      hue += hueStep;
+      freq *= next;
     }
   }
-  stops = stops.sort(([fa],[fb]) => fb - fa) // ensure stops are sorted in descending order
-  return 'linear-gradient(' + mode + ',' +
-    stops.map(([freq, color, w])=>{
-      if (!w) { w = 1/2 }
-      let stop = freqStops[freq] || (70.3 - 8.29*Math.log2(freq/62.5))      
-      return [
-        '#0000 ' + Number(stop - w).toFixed(2) + '%',
-        color + ' ' + stop + '%',
-        '#0000 ' + Number(stop + w).toFixed(2) + '%'
-      ].join(',')
-    }).join(',')
-  +')'
+  stops = stops.sort(([fa], [fb]) => fb - fa); // ensure stops are sorted in descending order
+  return (
+    'linear-gradient(' +
+    mode +
+    ',' +
+    stops
+      .map(([freq, color, w]) => {
+        if (!w) {
+          w = 1 / 2;
+        }
+        let stop = freqStops[freq] || 70.3 - 8.29 * Math.log2(freq / 62.5);
+        return [
+          '#0000 ' + Number(stop - w).toFixed(2) + '%',
+          color + ' ' + stop + '%',
+          '#0000 ' + Number(stop + w).toFixed(2) + '%',
+        ].join(',');
+      })
+      .join(',') +
+    ')'
+  );
 }
 
-// @name: useSpectrum
+// @name: useSpectrum(<Object> config | <Array> stops)
+//
+// @param: <Object> config
+// @param: lines <Int> : generate up to number of lines
+// @param: base <Int> : start frequency to draw line
+// @param: next <Number> : subsequent frequency will multiply by this multiplier
+// @param: alpha <Number> : transparency of lines
+// @param: hue <Int> : starting hue ranges between 0-360
+// @param: hueStep <Int> : hue spacing between each subsequent hue, lower numbers mean colors will be closer
+// @param: thickness <Number> : line thickness (in px)
+// @param: mul <Number> : subsequent line thickness multiplier, positive => subsequently thicker lines
+//
+// @param: <Array> stops, an array of stops with this following format
+// @param: <Array> stop : [frequency <Int>, CSScolor <string>, thickness <Number (optional)>]
 // @example:
-// useSpectrum(
-// {lines:4, base:1000, next:1/2, hue:40, hueStep:40, alpha:1},
-// [
-//   // [freq, color, width] --> setting this will override the config opts argument
+// useSpectrum({lines:4, base:1000, next:1/2, hue:40, hueStep:40, alpha:1});
+// // OR
+// useSpectrum([
 //   [1000,'oklch(.7 .2 40/1)',1],
 //   [250, 'oklch(.7 .2 120/1)',1],
 // ]);
+//
 // $:freq("<125 250 500 1000 2000 4000>*4").s("sine")
 //   .color("<blue cyan green yellow orange red>*4")
 //   .gain("<6 5 4 3 2 1>*4".div(4))._spectrum({width:800,speed:4})
-window.useSpectrum = function ({lines=7,base=2000,next=1/2,alpha=1,hue=40,hueStep=40}=opts,stops=[]) {
-  document.querySelectorAll('style').forEach(
-  n => n.append(
-  '#pre, .cm-widget-container>canvas{'+
-    'position:relative;'+
-    'transform: scaleY(175%) translateY(7%);'+
-    'z-index:-1;'+
-    'filter:saturate(1.5);'+
-    'background:'+lineargradient(stops,lines,base,next,alpha,hue,hueStep)+
-  '}'))
-}
+window.useSpectrum = function (opts) {
+  let stops = [];
+  const { lines = 7, base = 8000, next = 1 / 2, alpha = 1, hue = 40, hueStep = 40, thickness = 0.1, mul = 0.2 } = opts;
+  if (Array.isArray(opts)) {
+    stops = opts;
+  }
+  document
+    .querySelectorAll('style')
+    .forEach((n) =>
+      n.append(
+        '#pre, .cm-widget-container>canvas{' +
+          'position:relative;' +
+          'transform: scaleY(175%) translateY(7%);' +
+          'z-index:-1;' +
+          'filter:saturate(1.5);' +
+          'background:' +
+          lineargradient(stops, lines, base, next, alpha, hue, hueStep, thickness, mul) +
+          '}',
+      ),
+    );
+};
+
 
 // global functions
 window.blockArrange = function (patArr=[
